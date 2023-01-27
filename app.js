8ac4d467255699588f9836280f77facc4aec3344
Prins Kamariya <kamriyaprince@gmail.com>
2023-01-27 14:02:57 +0530

    Added Get req to find all products from db

--- app.js
+++ app.js
@@ -23,12 +23,21 @@ const productSchema = new mongoose.Schema({
 
 const Product = new mongoose.model("Product", productSchema);
 
-//Create Product
+// Create Product
 app.post("/api/v1/product/new", async (req, res) => {
   const product = await Product.create(req.body);
   res.status(200).json({
     success: true,
     product,
+  });
+});
+
+// Read Product
+app.get("/api/v1/products", async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    products,
   });
 });
 
